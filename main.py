afc7948c51ac768c7cdf2f2aad1a12ca020cd9b2
gianluca <morcaldi>
2026-06-09 11:37:45 +0200

    First augmentation added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import torch
 from torch.utils.data import DataLoader, Dataset
+import torchvision.transforms.v2 as transforms_v2
 import pydicom as dicom
 import pandas as pd
 import numpy as np
@@ -23,16 +24,23 @@ args = parser.parse_args()
 
 data_root_filepath = args.data_root_filepath
 
-learning_rate = 1e-3
+learning_rate = 5e-3
 batch_size = 20
 epochs = 10
 
+# defyning transforms to augment data
+train_transforms = test_transforms = transforms_v2.Compose(
+    [
+        transforms_v2.RandomHorizontalFlip()
+    ]
+)
+
 # loading data
-train_data = cbis.CBIS_Dataset(data_root_filepath=data_root_filepath, train=True)
-test_data = cbis.CBIS_Dataset(data_root_filepath=data_root_filepath, train=False)
+train_data = cbis.CBIS_Dataset(data_root_filepath=data_root_filepath, train=True, transform=train_transforms)
+test_data = cbis.CBIS_Dataset(data_root_filepath=data_root_filepath, train=False, transform=test_transforms)
 
 train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
+test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
 # defyning loss function
 loss_fn = unet.dice_loss
